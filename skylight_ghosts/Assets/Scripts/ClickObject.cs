068f2114a68d5358d824b84b41128b4fc7fd8b23
Kelly D'oleo <kellydeoleo@gmail.com>
2023-04-02 18:14:07 -0400

    Add point system and picture frame addition

--- skylight_ghosts/Assets/Scripts/ClickObject.cs
+++ skylight_ghosts/Assets/Scripts/ClickObject.cs
@@ -7,54 +7,48 @@ using TMPro;
 public class ClickObject : MonoBehaviour
 {
 
+    public PlayerPoints points; 
     public Image objectToTrigger;
     public string displayText;
     public TMP_Text textObject;
-    //private Vector3 originalPosition;
-    //private Vector3 originalTextPosition;
-    private Transform objectClicked;
-    //private Camera mainCamera;
+    private List<string> items = new List<string>();
+
+    //{ "Hat4", "Cigarette Box","Lamp","Blood", "Box with Vase", "Closed Box on Left", "Closed Box on Right", "Suitcase", "Candle Holder" };
 
     private bool isClicked = false;
-    //private float backgroundPositionRate = 9.7f;
-    //private float textPositionRate = 9f;
-    
 
     private void Start()
     {
-        //mainCamera = Camera.main;
         objectToTrigger.enabled = false;
         textObject.enabled = false;
-        //originalPosition = objectToTrigger.transform.position;
-        //originalTextPosition = textObject.transform.position;
-        //objectClicked = GetComponent<Transform>();
-        //Debug.Log("ScreenToWorldPoint x");
-        //Debug.Log(mainCamera.ScreenToWorldPoint(new Vector3(0, originalPosition.y, originalPosition.z)).x);
     }
-
 
 
     void OnMouseDown()
     {
         if (!isClicked)
         {
-
-
-            //objectToTrigger.transform.position = new Vector3((float)mainCamera.ScreenToWorldPoint(new Vector3(0, originalPosition.y, originalPosition.z)).x - backgroundPositionRate, originalPosition.y, originalPosition.z);
-            //textObject.transform.position = new Vector3((float)mainCamera.ScreenToWorldPoint(new Vector3(0, originalTextPosition.y, originalTextPosition.z)).x - textPositionRate, originalTextPosition.y, originalTextPosition.z);
-            //Debug.Log(objectToTrigger.transform.position.x);
+            Debug.Log("this.gameObject");
+            Debug.Log(this.gameObject.tag);
             objectToTrigger.enabled = true;
             textObject.enabled = true;
             textObject.text = displayText;
             isClicked = true;
+            if(!items.Contains(this.gameObject.tag))
+            {
+                points.totalPoints += 1;
+            }
+            items.Add(this.gameObject.tag);
+            Debug.Log(points.totalPoints);
+           
+           
+
         }
         else
         {
             objectToTrigger.enabled = false;
             textObject.text = "";
             textObject.enabled = false;
-
-            //objectToTrigger.transform.position = originalPosition;
             isClicked = false;
         } 
     }
